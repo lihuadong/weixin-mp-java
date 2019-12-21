5b13bb108955a8b7009671e109d503c736bbae3f
lihuadong <lihuadongv@163.com>
2019-12-22 00:34:34 +0800

    修改代码注释

--- src/weixin/statistics/StatisticsManager.java
+++ src/weixin/statistics/StatisticsManager.java
@@ -1,19 +1,20 @@
 /**
- * 包到位小程序SaaS
+ * 
+ * 微信-公众号-封装接口JAVA版本
  * weixin.statistics
  * StatisticsManager.java
  * Ver0.0.1
  * 2016年1月12日-下午10:25:53
- *  2014-2019 全智道(北京)科技有限公司
+ * 2014-2019  ©全智道(北京)科技有限公司
  * 
  */
+
 package weixin.statistics;
 
 /**
  * 
  * StatisticsManager
  * 
- * 李华栋
  * 李华栋
  * 2016年1月12日 下午10:25:53
  * 
