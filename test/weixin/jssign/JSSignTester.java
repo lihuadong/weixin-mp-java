f3e02eb5dc88297385647e4d53b2f8e83146fc72
lihuadong <lihuadongv@163.com>
2019-12-27 20:27:19 +0800

    js签名类调整

---
+++ test/weixin/jssign/JSSignTester.java
@@ -0,0 +1,18 @@
+package weixin.jssign;
+
+import weixin.jssdk.JSSign;
+import java.util.Map;
+
+public class JSSignTester {
+
+    
+    public static void main(String[] args) {
+        String jsapi_ticket = "bxLdikRXVbTPdHSM05e5u4RbEYQn7pNQMPrfzl8lJNb1foLDa3HIwI3BRMkQmSO_5F64VFa75uURcq6Uz7QHgA";
+        String url = "http://omstest.vmall.com:23568/thirdparty/wechat/vcode/gotoshare?quantity=1&batchName=MATE7";
+        Map<String, String> ret = JSSign.sign(jsapi_ticket, url);
+        for (Map.Entry entry : ret.entrySet()) {
+            System.out.println(entry.getKey() + ", " + entry.getValue());
+        }
+    }
+
+}
