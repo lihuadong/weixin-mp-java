a2059fd9df144942ce14a248ec6e36a465e58885
lihuadong <lihuadongv@163.com>
2020-01-02 21:59:54 +0800

    订阅接口调整

--- src/weixin/base/APIURL.java
+++ src/weixin/base/APIURL.java
@@ -79,8 +79,7 @@ public class APIURL {
 		 * 通过API推送订阅模板消息给到授权微信用户
 		 */
 		public  final static  String  MSG_SUBSCRIBE_URL  =CURRENT_API+"cgi-bin/message/template/subscribe?access_token=";
-			
-			
+		
 		/****************************************************************************************************
 		 * 
 		 * 微信卡券
