16081559bbb8e8e1e2e4f41548e4fa63320e3ac2
lihuadong <lihuadongv@163.com>
2020-01-04 23:17:03 +0800

    StringUtil测试类添加

---
+++ test/weixin/util/StringUtilTester.java
@@ -0,0 +1,10 @@
+package weixin.util;
+
+public class StringUtilTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println(StringUtil.getNonceStr());
+	}
+
+}
