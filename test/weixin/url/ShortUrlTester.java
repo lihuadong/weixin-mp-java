1dc0e605a50eeb744ba79c4f02cf50a5e09bc018
lihuadong <lihuadongv@163.com>
2020-01-10 23:53:48 +0800

    测试短链接当天使用量

--- test/weixin/url/ShortUrlTester.java
+++ test/weixin/url/ShortUrlTester.java
@@ -6,18 +6,18 @@ public class ShortUrlTester {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        String at = "29_EQZDjd6nKjlSbr67LZ4xIlNLI6gRtRpB-oTivxowcURT_sN3jgPXluuGJ_NLyS0cy12p7BIhUVOJvdTW_r9NhmTVdXFN90rZ9klzsV4jlSTubzFuvAekIUp1URKnR_Z4dTFzZdv2cKe-BbQRQBEfAHAALO"; 
+        String at = "29_PCsGiqzohiNtaMtYR1KIY6t9l7SSfX7_DIc4eregVQpJTHfW25xKxRkfberje6uYawVRTbzQ7Vq9dLKQLZL_Tm6iW8OYvSaP3kA-dRuH1NHIPNbeGbbfI11XX6NYthBbUjAElfkHdgCAgfWjIGMeAJACAG"; 
         
         ShortUrlManager  manager  = new ShortUrlManager(at);
-        String url  = manager.getShortUrl("https://org.modao.cc/app/ad9559a5bafb09eaf6827a3cfe28e725cf5345a5");
-        System.out.println(url);
+        //String url  = manager.getShortUrl("https://org.modao.cc/app/ad9559a5bafb09eaf6827a3cfe28e725cf5345a5");
+        //System.out.println(url);
         
-//        int i = 1000;
-//        while(true) {
-//        	i--;
-//        	url  = manager.getShortUrl("https://wukonglai.com/index.html?num="+i);
-//            System.out.println(url);
-//        }
+        int i = 100;
+        while(true) {
+        	i--;
+        	String url  = manager.getShortUrl("https://wukonglai.com/index.html?num="+i);
+            System.out.println(i+":"+url);
+        }
         
 	}
 
