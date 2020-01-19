a3212f763454ca7163327d3a8e0d5ad99143bf30
lihuadong <lihuadongv@163.com>
2020-01-19 23:59:06 +0800

    规范类名JSON

---
+++ src/weixin/util/JSONUtil.java
@@ -0,0 +1,39 @@
+package weixin.util;
+
+public class JSONUtil {
+
+	  private static final Logger LOGGER = LoggerFactory.getLogger(JSONUtil.class);
+
+	    private static final ObjectMapper OBJECT_MAPPER =new ObjectMapper();
+
+	    /**
+	     * 将POJO转换为JSON
+	     */
+	    public static <T> String toJson(T obj){
+	        String json;
+	        try {
+	            json = OBJECT_MAPPER.writeValueAsString(obj);
+	        } catch (JsonProcessingException e) {
+	            LOGGER.error("convert POJO to JSON failure",e);
+	            throw new RuntimeException(e);
+	            //e.printStackTrace();
+	        }
+	        return json;
+	    }
+
+	    /**
+	     * 将JSON转为POJO
+	     */
+	    public static <T> T fromJson(String json,Class<T> type){
+	        T pojo;
+	        try {
+	            pojo = OBJECT_MAPPER.readValue(json,type);
+	        } catch (IOException e) {
+	            LOGGER.error("convert JSON to POJO failure",e);
+	            throw new RuntimeException(e);
+	            //e.printStackTrace();
+	        }
+	        return pojo;
+
+	    }
+}
