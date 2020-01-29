4f2426619e35bce49fc992c653d546df72a02326
lihuadong <lihuadongv@163.com>
2020-01-29 21:56:50 +0800

    JSON引入新类型

--- src/weixin/util/JSONUtil.java
+++ src/weixin/util/JSONUtil.java
@@ -1,7 +1,12 @@
 package weixin.util;
 
+import org.json.HTTP;
+
+import java.io.IOException;
+
 import org.apache.log4j.Logger;
-import org.apache.log4j.spi.LoggerFactory;
+import org.codehaus.jackson.JsonProcessingException;
+import org.codehaus.jackson.map.ObjectMapper;
 
 public class JSONUtil {
 
@@ -13,14 +18,17 @@ public class JSONUtil {
 	     * 将POJO转换为JSON
 	     */
 	    public static <T> String toJson(T obj){
-	        String json;
+	        String json = null;
 	        try {
 	            json = OBJECT_MAPPER.writeValueAsString(obj);
 	        } catch (JsonProcessingException e) {
 	            LOGGER.error("convert POJO to JSON failure",e);
 	            throw new RuntimeException(e);
 	            //e.printStackTrace();
-	        }
+	        } catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 	        return json;
 	    }
 
